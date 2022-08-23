863345436fa965ee3a1df47a471e2327bffae218
WJF <819708962@qq.com>
2022-08-23 20:09:19 +0800

    8.23晚

--- teledemo/peoplemanage/src/main/java/teleDemo/entities/PostVo.java
+++ teledemo/peoplemanage/src/main/java/teleDemo/entities/PostVo.java
@@ -1,42 +1,18 @@
 package teleDemo.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostVo<T>{
     private int code;
     private String msg;
     private List<T> data;
-
-    public PostVo(int code, String msg, List<T> data) {
-        this.code = code;
-        this.msg = msg;
-        this.data = data;
-    }
-
-    public PostVo() {
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public List<T> getData() {
-        return data;
-    }
-
-    public void setData(List<T> data) {
-        this.data = data;
-    }
 }
